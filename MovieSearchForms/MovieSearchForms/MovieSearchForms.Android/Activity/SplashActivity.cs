ec674fec89f53b0adcba4dc952fabadc7500a30f
Jon Sigmundsson <jonhei13@ru.is>
2017-12-11 12:59:12 +0000

    splash activity

---
+++ MovieSearchForms/MovieSearchForms/MovieSearchForms.Android/Activity/SplashActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;
+using MovieSearch.MovieApiService;
+using MovieSearchForms.Droid;
+
+namespace MovieSearch.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, Icon = "@drawable/icon")]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            MainActivity.MovieService = new MovieSearchService();
+            // Create your application here
+            this.StartActivity(typeof(MainActivity));
+            this.Finish();
+        }
+    }
+}
