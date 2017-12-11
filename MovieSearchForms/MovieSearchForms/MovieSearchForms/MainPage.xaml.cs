0dc9858eed4ac626a6c282ded7d60fbd6b40318a
Jon Sigmundsson <jonhei13@ru.is>
2017-12-11 12:16:36 +0000

    MOvieSearch BUtton

--- MovieSearchForms/MovieSearchForms/MovieSearchForms/MainPage.xaml.cs
+++ MovieSearchForms/MovieSearchForms/MovieSearchForms/MainPage.xaml.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using MovieSearch.MovieApiService;
 
 namespace MovieSearchForms
 {
     public partial class MainPage : ContentPage
     {
+        private MovieSearchService _service;
         public MainPage()
         {
             InitializeComponent();
+            _service = new MovieSearchService();
+        }
+        private async void TitleSearchButton_OnClicked(object sender, EventArgs e)
+        {
+
+            var Movies =  await _service.GetMoviesByTitle(this.TitleSearch.Text);
+            this.MovieLabel.Text = (from x in Movies select x.Title).FirstOrDefault();
+            
         }
     }
 }
--- MovieSearchForms/MovieSearchForms/MovieSearchForms/obj/Debug/MovieSearchForms.MainPage.xaml.g.cs
+++ MovieSearchForms/MovieSearchForms/MovieSearchForms/obj/Debug/MovieSearchForms.MainPage.xaml.g.cs
@@ -16,8 +16,20 @@ namespace MovieSearchForms {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Entry TitleSearch;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Button TitleSearchButton;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Label MovieLabel;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            TitleSearch = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "TitleSearch");
+            TitleSearchButton = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "TitleSearchButton");
+            MovieLabel = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "MovieLabel");
         }
     }
 }
