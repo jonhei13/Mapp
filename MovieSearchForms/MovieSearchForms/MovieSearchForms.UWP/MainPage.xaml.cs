57fa485ca50cda6707792bb067319494ff311b4e
Jon Sigmundsson <jonhei13@ru.is>
2017-12-13 11:49:40 +0000

    ListView Select fixed

--- MovieSearchForms/MovieSearchForms/MovieSearchForms.UWP/MainPage.xaml.cs
+++ MovieSearchForms/MovieSearchForms/MovieSearchForms.UWP/MainPage.xaml.cs
@@ -1,18 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
+﻿
 namespace MovieSearchForms.UWP
 {
     public sealed partial class MainPage
@@ -20,8 +6,8 @@ namespace MovieSearchForms.UWP
         public MainPage()
         {
             this.InitializeComponent();
-
-            LoadApplication(new MovieSearchForms.App());
+            
+            LoadApplication(new MovieSearchForms.Pages.App());
         }
     }
 }
